7276cdd68430c4d42a34df217cd07d8cb6757e1d
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-11-17 14:27:35 -0300

    Delete src/main/java/net/soggart/potionmod directory

--- src/main/java/net/soggart/potionmod/item/custom/FilledSyringeItem.java
+++
@@ -1,9 +0,0 @@
-package net.soggart.potionmod.item.custom;
-
-import net.minecraft.item.Item;
-
-public class FilledSyringeItem extends Item {
-    public FilledSyringeItem(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/net/soggart/potionmod/mixin/PlayerEntityExt.java
+++
@@ -1,5 +0,0 @@
-package net.soggart.potionmod.mixin;
-
-public interface PlayerEntityExt {
-    void addKills(int amount);
-}
