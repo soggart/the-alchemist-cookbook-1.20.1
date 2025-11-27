53be520d8d968205e3614067191938357ff4a835
soggart <confusedsavior@gmail.com>
2025-11-27 00:37:58 -0300

    a

---
+++ src/main/java/net/soggart/alchemistcookbook/utils/EnergyUtil.java
@@ -0,0 +1,5 @@
+package net.soggart.alchemistcookbook.utils;
+
+public class EnergyUtil { //AQUI A GNT VAI BOTAR FUNÇÕES PRA AJUDAR A IMPLEMENTAR O SISTEMA DE ENERGIA
+    //SE VC N SOUBER DO Q EU TÔ FALANDO EU EXPLICO DPS
+}
